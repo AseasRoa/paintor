b3041d11535181741b4b26ff326796fdebcfd87a
AseasRoa <aseasroa@proton.me>
2023-11-22 15:58:18 +0200

    Moved "types" dir

--- types/app.d.ts
+++ types/app.d.ts
@@ -45,6 +45,6 @@ type Subscription = {
    * Used for cleaning up subscriptions from the DOM element itself,
    * when the element is being removed
    */
-  stateSubscription: import('../StateProxySubscriptions').StateProxySubscriptions;
+  stateSubscription: import('../src/StateProxySubscriptions').StateProxySubscriptions;
   statePath: string;
 }
