829fd62b0be04122839a2dda35f3dc750af92a06
AseasRoa <aseasroa@proton.me>
2024-02-06 14:54:53 +0200

    docs removed | docs moved to another branch

--- docs/v1.x/internationalization/translations-1.js
+++
@@ -1,7 +0,0 @@
-import { component } from 'paintor'
-
-const translationEn = { GREETING: 'Hello!', QUESTION: 'How are you today?' }
-
-component(($) => {
-  $.div('GREETING', ' ', 'QUESTION')
-}).useTranslations(translationEn).paint('translations-1')
