dcc75508da73d67f745e03698549ecf5cb9d4d81
AseasRoa <aseasroa@proton.me>
2024-02-27 14:50:59 +0200

    refactor: Subscriptions overhaul

--- lib/types/app.d.ts
+++ lib/types/app.d.ts
@@ -5,7 +5,7 @@ type TargetObject = Object<any, any> | Array<any>
  * A function that binds the property of
  * an HTML element with an element from a state
  */
-type BindFunction = (element?: Node | undefined) => any
+type BindFn = (element?: Node | undefined) => any
 type CallbackForFunction = (
   (
     values: any[],
@@ -18,37 +18,26 @@ type CallbackForFunction = (
 /**
  * Used in "if" statement for the condition parameter
  */
-type StatementBindFunction = (element?: HTMLElement | undefined) => boolean
+type StatementBindFn = (element?: HTMLElement | undefined) => boolean
 
 type ForLoopCallback<T> = (value?: T | undefined, key?: string | number | undefined) => boolean | undefined | Template | Component
 type ForLoopCallbackOnEmpty = () => undefined
 type ForLoopIterableCallback = (index?: number | undefined) => boolean | undefined
 
-type RepaintFunctionForFunction = (data: boolean | number | State) => void
-type RepaintFunctionForState = (
+type RepaintFnForFunction = (data: boolean | number | State) => void
+type RepaintFnForState = (
   action: number,
   updatedObject: TargetObject,
   updatedState: State,
   prop: string | symbol,
   arrayFunctionArgs?: any[],
 ) => void
-type RepaintFunction = RepaintFunctionForFunction | RepaintFunctionForState
+type RepaintFn = RepaintFnForFunction | RepaintFnForState
 
-/**
- * A single subscriptions record
- */
+type StateProp = string | symbol
 type Subscription = {
-  propertyName: string | symbol,
-  subPropertyName: string,
-  bindFunction: BindFunction,
-  /**
-   * Only used in the if() function
-   */
-  repaintFunction: null | RepaintFunctionForFunction | RepaintFunctionForState,
-  /**
-   * Used for cleaning up subscriptions from the DOM element itself,
-   * when the element is being removed
-   */
-  subscriptionsInstance: import('../state/Subscriptions').Subscriptions,
-  state: State
+  element: HtmlElement,
+  elementProp: string | symbol,
+  elementInnerProp: string,
+  repaintFn: null | RepaintFnForFunction | RepaintFnForState
 }
