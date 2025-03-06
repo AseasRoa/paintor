ca89c1d8499b8c531382c3959ccb3d032308def4
AseasRoa <aseasroa@proton.me>
2025-03-06 20:04:57 +0200

    fix: Better handling of styles in css() and $css()

--- dev/Counter.js
+++ dev/Counter.js
@@ -1,10 +1,13 @@
-import { state, template } from '#paintor'
+import { css, state, template } from '#paintor'
 
 export const Counter = () => {
-  return template(({ button, div }) => {
+  //css(`button{color:red}`)
+
+  return template(({ $css, button, div }) => {
+    $css(`button{color:red}`)
     const localState = state({ clicks: 0 })
 
-    div(
+    div({ className: 'class' },
       button({ onClick: () => localState.clicks += 1 }, 'Click me'),
       div(() => localState.clicks)
     )
