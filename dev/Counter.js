02496a833c4d1db00a821191b051f78740140e47
AseasRoa <aseasroa@proton.me>
2025-04-25 13:03:52 +0300

    refactor: dev files

--- dev/Counter.js
+++ dev/Counter.js
@@ -1,15 +1,16 @@
 import { css, state, template } from '#paintor'
 
 export const Counter = () => {
-  //css(`button{color:red}`)
+  // css(`button { color: red; }`)
 
-  return template(({ $css, button, div }) => {
-    $css(`button{color:red}`)
+  return template((x) => {
+    x.$css('button { color: red; }')
+
     const localState = state({ clicks: 0 })
 
-    div({ className: 'class' },
-      button({ onClick: () => localState.clicks += 1 }, 'Click me'),
-      div(() => localState.clicks)
+    x.div({ className: 'class' },
+      x.button({ onClick: () => localState.clicks += 1 }, 'Click me'),
+      x.div(() => localState.clicks)
     )
   })
 }
